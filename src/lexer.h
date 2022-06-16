c0892c8f809e3235c1c46a28f343b5319baab6df
Eduardo Antunes dos Santos Vieira <eduardoantunes986@gmail.com>
2022-06-16 14:03:53 -0300

    Add license
    GNU is love, GNU is life

--- src/lexer.h
+++ src/lexer.h
@@ -1,3 +1,27 @@
+/*
+   Copyright 2022 Eduardo Antunes dos Santos Vieira
+
+   This file is part of cog.
+
+   cog is free software: you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation,
+   either version 3 of the License, or (at your option)
+   any later version.
+
+   cog is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied
+   warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+   PURPOSE. See the GNU General Public License for more
+   details.
+
+   You should have received a copy of the GNU General
+   Public License along with cog. If not, see
+   <https://www.gnu.org/licenses/>.
+*/
+
+// Lexing analysis component of cog
+
 #ifndef COG_LEXER_H
 #define COG_LEXER_H
 
